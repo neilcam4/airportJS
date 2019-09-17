f9b428af34f509207f93defef09df990b8c0539b
Neil Campbell <neildcampbell@hotmail.com>
2019-09-17 15:39:39 +0100

    added more tests takeoff method

--- jasmine-standalone-3.4.0/spec/AirportSpec.js
+++ jasmine-standalone-3.4.0/spec/AirportSpec.js
@@ -14,5 +14,10 @@ describe('Airport', function(){
    airport.clearForLanding(plane);
    expect(airport.planes()).toEqual([plane]);
   });
+  it('can clear planes for takeoff', function(){
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
 
 });
--- jasmine-standalone-3.4.0/src/airport.js
+++ jasmine-standalone-3.4.0/src/airport.js
@@ -8,3 +8,6 @@ Airport.prototype.planes = function(){ return this._hangar; };
 Airport.prototype.clearForLanding = function(plane){
    this._hangar.push(plane)
 };
+Airport.prototype.clearForTakeOff = function(plane){
+  this._hangar = [];
+};
